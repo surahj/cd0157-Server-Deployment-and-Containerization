0a114d68487c3fde7f97b853152863155b7ea5e3
surahj <odebodeopeyemi@gmail.com>
2022-10-08 05:17:17 +0100

    Update test file

--- test_main.py
+++ test_main.py
@@ -16,6 +16,7 @@ PASSWORD = 'huff-puff'
 def client():
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
+    assert False
     client = main.APP.test_client()
 
     yield client
@@ -31,7 +32,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
